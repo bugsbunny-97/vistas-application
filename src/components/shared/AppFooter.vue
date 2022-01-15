6a7d0bf489571181c2f9a85152659984fcc74d93
Utkarsh Gupta <utkarsh.elia@gmail.com>
2022-01-15 21:09:33 +0530

    Complete file structure with Vuejs template

---
+++ src/components/shared/AppFooter.vue
@@ -0,0 +1,99 @@
+<template>
+	<!-- App footer -->
+	<div class="container mx-auto ">
+		<div
+			class="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark"
+		>
+			<!-- Footer social links -->
+			<div
+				class="flex flex-col justify-center items-center mb-12 sm:mb-28"
+			>
+				<p
+					class="text-3xl sm:text-4xl font-semibold text-primary-dark dark:text-primary-light mb-5"
+				>
+					Follow me
+				</p>
+				<ul class="flex gap-4 sm:gap-8">
+					<a
+						v-for="social in socials"
+						:key="social.id"
+						:href="social.url"
+						target="__blank"
+						class="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-lg bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm∫ p-4"
+					>
+						<i
+							:data-feather="social.icon"
+							class="w-5 sm:w-8 h-5 sm:h-8"
+						></i>
+					</a>
+				</ul>
+			</div>
+
+			<!-- Footer copyright -->
+			<div class="flex justify-center items-center text-center">
+				<div class="text-lg text-ternary-dark dark:text-ternary-light ">
+					&copy; {{ copyrightDate }}.
+					<a
+						href="https://github.com/realstoman/vuejs-tailwindcss-portfolio"
+						target="__blank"
+						class="hover:underline"
+					>
+						Vue.js & TailwindCSS Portfolio
+					</a>
+					.
+					<a
+						href="https://stoman.me"
+						target="__blank"
+						class="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline"
+						>Stoman</a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import feather from 'feather-icons';
+export default {
+	data() {
+		return {
+			copyrightDate: new Date().getFullYear(),
+			socials: [
+				{
+					id: 1,
+					name: 'GitHub',
+					icon: 'github',
+					url: 'https://github.com/realstoman',
+				},
+				{
+					id: 2,
+					name: 'Twitter',
+					icon: 'twitter',
+					url: 'https://twitter.com/realstoman',
+				},
+				{
+					id: 3,
+					name: 'Medium',
+					icon: 'book',
+					url: 'https://stoman.medium.com',
+				},
+				{
+					id: 4,
+					name: 'Instagram',
+					icon: 'instagram',
+					url: 'https://instagram.com/realstoman',
+				},
+			],
+		};
+	},
+	mounted() {
+		feather.replace();
+	},
+	updated() {
+		feather.replace();
+	},
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="container mx-auto">
+		<!-- Banner -->
+		<AppBanner class="mb-5 sm:mb-8" />
+
+		<!-- Projects -->
+		<ProjectsGrid />
+
+		<!-- Load more projects button -->
+		<div class="mt-10 sm:mt-20 flex justify-center">
+			<router-link
+				to="/projects"
+				class="flex items-center px-6 py-3 rounded-xl shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl font-medium"
+				aria-label="More Projects"
+				>More Projects</router-link
+			>
+		</div>
+	</div>
+</template>
+
+<script>
+import AppBanner from '@/components/shared/AppBanner';
+import ProjectsGrid from '../components/projects/ProjectsGrid.vue';
+
+export default {
+	name: 'Home',
+	components: {
+		AppBanner,
+		ProjectsGrid,
+	},
+};
+</script>
+
+<style scoped></style>
